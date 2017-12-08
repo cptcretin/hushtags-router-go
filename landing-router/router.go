6569e48fd5be56f7400fe71189f8fdb772fc279b
Justin McCullough <justin@cretin.me>
2017-12-08 16:06:00 +0000

    Added demo tag support.

--- landing-router/router.go
+++ landing-router/router.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
-    
-    "github.com/gorilla/mux"
-    "github.com/urfave/negroni"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 func main() {
@@ -19,15 +19,24 @@ func main() {
 func CreateServer() *http.Server {
 	r := mux.NewRouter().StrictSlash(false)
 
-    r.HandleFunc("/{tagHandle}", serveTag).Methods("GET")
+	r.HandleFunc("/{tagHandle}", serveTag).Methods("GET")
 
 	n := negroni.New()
-    n.Use(negroni.NewStatic(http.Dir("www/")))
+	n.Use(negroni.NewStatic(http.Dir("www/")))
 	n.UseHandler(r)
 
 	return &http.Server{Addr: ":" + os.Getenv("PORT"), Handler: n}
 }
 
 func serveTag(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "www/index.html")    
+	handle := mux.Vars(r)["tagHandle"]
+
+	if handle == "dnKccFf" ||
+		handle == "iIdKcpM" ||
+		handle == "bCiVZJn" {
+
+		http.Redirect(w, r, "https://hushtags-dev.herokuapp.com/multiple-tags"+r.URL.Path, http.StatusTemporaryRedirect)
+	} else {
+		http.ServeFile(w, r, "www/index.html")
+	}
 }
