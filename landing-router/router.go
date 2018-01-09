f8d6ad05613cab3adedd7716d420e9d0286d0fd7
Justin McCullough <justin@cretin.me>
2018-01-09 07:06:07 +0000

    Testing support for Android intents.

--- landing-router/router.go
+++ landing-router/router.go
@@ -20,6 +20,7 @@ func CreateServer() *http.Server {
 	r := mux.NewRouter().StrictSlash(false)
 
 	r.HandleFunc("/{tagHandle}", serveTag).Methods("GET")
+	r.HandleFunc("/.well-known/assetlinks.json", serveAssetLinks).Methods("GET")
 
 	n := negroni.New()
 	n.Use(negroni.NewStatic(http.Dir("www/")))
@@ -41,4 +42,10 @@ func serveTag(w http.ResponseWriter, r *http.Request) {
 	} else {
 		http.ServeFile(w, r, "www/index.html")
 	}
+}
+
+func serveAssetLinks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Size", "300")
+	http.ServeFile(w, r, "./assetlinks.json")
 }
