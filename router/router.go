4ebafe7b5a41932feb65e88cd30e079853a1abcf
Justin McCullough <justin@cretin.me>
2018-01-19 21:20:40 +0000

    First draft of the new HushTags home page.

---
+++ router/router.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+type AppRootHandler struct{}
+
+func (h AppRootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "www/index.html")
+}
+
+func main() {
+	server := CreateServer()
+
+	log.Printf("Listening on %s...", server.Addr)
+	server.ListenAndServe()
+}
+
+func CreateServer() *http.Server {
+	r := mux.NewRouter().StrictSlash(false)
+	rootHandler := AppRootHandler{}
+
+	r.PathPrefix("/discover-tag").Handler(rootHandler).Methods("GET")
+	r.PathPrefix("/multiple-tags").Handler(rootHandler).Methods("GET")
+	r.PathPrefix("/single-tags").Handler(rootHandler).Methods("GET")
+	r.PathPrefix("/view-media").Handler(rootHandler).Methods("GET")
+	r.PathPrefix("/cart").Handler(rootHandler).Methods("GET,POST")
+	r.PathPrefix("/store").Handler(rootHandler).Methods("GET,POST")
+	r.PathPrefix("/report-content").Handler(rootHandler).Methods("GET")
+	r.PathPrefix("/report-successful").Handler(rootHandler).Methods("GET")
+	r.PathPrefix("/error").Handler(rootHandler).Methods("GET")
+	r.PathPrefix("/help").Handler(rootHandler).Methods("GET")
+	r.PathPrefix("/author").Handler(rootHandler).Methods("GET")
+
+	r.HandleFunc("/{tagHandle}", serveTag).Methods("GET")
+	r.HandleFunc("/.well-known/assetlinks.json", serveAssetLinks).Methods("GET")
+
+	n := negroni.New()
+	n.Use(negroni.NewStatic(http.Dir("www/")))
+	n.UseHandler(r)
+
+	return &http.Server{Addr: ":" + os.Getenv("PORT"), Handler: n}
+}
+
+func serveTag(w http.ResponseWriter, r *http.Request) {
+	h := mux.Vars(r)["tagHandle"]
+	http.Redirect(w, r, "/discover-tag/"+h, http.StatusTemporaryRedirect)
+}
+
+func serveAssetLinks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Size", "300")
+	http.ServeFile(w, r, "./assetlinks.json")
+}
